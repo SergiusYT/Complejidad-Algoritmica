96484510a8a789622297495128b2d32bc49bae6b
Sergio Lozano <145393538+SergiusYT@users.noreply.github.com>
2024-08-23 18:05:09 -0500

    Update Profesor.java

--- Corte1/Taller2/Profesor.java
+++ Corte1/Taller2/Profesor.java
@@ -1,14 +1,24 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class Main
-{  
-  public static void main( String[] args ){
-    Scanner entrada = new Scanner(System.in);
-    System.out.print("Valor de -n-: ");
-    int n = entrada.nextInt();
-    for(int exp=0; exp<= n; exp++){
-      System.out.println(" 2 Elevado a "+ exp + " igual a "+ (1 << exp));
+public class Profesor {  
+    public static void main(String[] args) {
+        long inicio = System.nanoTime();
+
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Valor de -n-: ");
+        int n = entrada.nextInt();
+
+        for (int exp = 0; exp <= n; exp++) {
+            System.out.println("2 Elevado a " + exp + " igual a " + (1 << exp));
+        }
+
+        entrada.close();
+
+        
+        long finalizacion = System.nanoTime();
+
+        
+        long duracion = finalizacion - inicio;
+        System.out.println("\nEl algoritmo se ejecutó en: " + duracion + " nanosegundos");
     }
-   }
-}  
-  
+}
