e70c2b167609c2d193cf1e936ce6a2030dad6699
Sergio Lozano <145393538+SergiusYT@users.noreply.github.com>
2024-08-23 18:06:09 -0500

    Update IterativoPow.java

--- Corte1/Taller2/IterativoPow.java
+++ Corte1/Taller2/IterativoPow.java
@@ -1,24 +1,35 @@
 import java.util.Scanner;
-import java.lang.Math;
-public class Taller2 {
-	
 
-	  public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
+public class IterativoPow {
+    public static void main(String[] args) {
+        long inicio = System.nanoTime();
 
-	    System.out.print("Ingrese un número entero positivo <= 20: ");
-	    int n = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
 
-	    if (n <= 0 || n > 20) {
-	      System.out.println("Error: El número debe ser entero positivo y menor o igual a 20.");
-	      return;
-	    }
+        System.out.print("Ingrese un número entero positivo <= 20: ");
+        int n = scanner.nextInt();
 
-	    for (int i = 0; i <= n; i++) {
-	       int resultado = (int) Math.pow(2, i);
-	      System.out.println("2 ^ " + i + " = " + resultado);
-	    }
-	  }
-	
+        if (n <= 0 || n > 20) {
+            System.out.println("Error: El número debe ser entero positivo y menor o igual a 20.");
+			
+            long finalizacion = System.nanoTime();
+            
+            long duracion = finalizacion - inicio;
+            System.out.println("\nEl algoritmo se ejecutó en: " + duracion + " nanosegundos");
+			scanner.close();
+            return;
+        }
 
+        for (int i = 0; i <= n; i++) {
+            int resultado = (int) Math.pow(2, i);
+            System.out.println("2 ^ " + i + " = " + resultado);
+        }
+
+        scanner.close();
+
+        long finalizacion = System.nanoTime();
+
+        long duracion = finalizacion - inicio;
+        System.out.println("\nEl algoritmo se ejecutó en: " + duracion + " nanosegundos");
+    }
 }
