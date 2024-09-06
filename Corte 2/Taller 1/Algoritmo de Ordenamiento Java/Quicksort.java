7d7c9bb84bcc25c53ca7c550770b2499dd7f3b93
alejaec <149398493+alejaec@users.noreply.github.com>
2024-09-05 21:14:07 -0500

    Update Quicksort.java
    Código de Ordenamiento de Quicksort en Java

--- Corte 2/Taller 1/Algoritmo de Ordenamiento Java/Quicksort.java
+++ Corte 2/Taller 1/Algoritmo de Ordenamiento Java/Quicksort.java
@@ -1,1 +1,68 @@
+import java.util.Random;
+
+public class Quicksort {
+    static final int N = 1000;
+
+    public static int partition(int[] arr, int low, int high, int[] comparisons, int[] swaps) {
+        int pivot = arr[high];
+        int i = low - 1;
+        int temp;
+
+        for (int j = low; j <= high - 1; j++) {
+            comparisons[0]++;
+            if (arr[j] < pivot) {
+                i++;
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                swaps[0]++;
+            }
+        }
+        temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        swaps[0]++;
+        return i + 1;
+    }
+
+    public static void quickSort(int[] arr, int low, int high, int[] comparisons, int[] swaps) {
+        if (low < high) {
+            int pi = partition(arr, low, high, comparisons, swaps);
+
+            quickSort(arr, low, pi - 1, comparisons, swaps);
+            quickSort(arr, pi + 1, high, comparisons, swaps);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[N];
+        int[] comparisons = {0};
+        int[] swaps = {0};
+
+        Random rand = new Random();
+        for (int i = 0; i < N; i++) {
+            arr[i] = rand.nextInt(10); 
+        }
+
+        System.out.println("Numeros generados:");
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        quickSort(arr, 0, N - 1, comparisons, swaps);
+
+        System.out.println("Numeros ordenados:");
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Comparaciones: " + comparisons[0]);
+        System.out.println("Intercambios: " + swaps[0]);
+        System.out.println("Estable: No");
+        System.out.println("Insercion: No");
+    }
+}
+
 
