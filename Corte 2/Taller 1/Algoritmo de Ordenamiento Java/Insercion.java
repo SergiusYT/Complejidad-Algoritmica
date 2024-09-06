d4c9fa2dfde3c17faa8ed9793d572ea5d47b1be8
alejaec <149398493+alejaec@users.noreply.github.com>
2024-09-05 21:08:27 -0500

    Update Insercion.java

--- Corte 2/Taller 1/Algoritmo de Ordenamiento Java/Insercion.java
+++ Corte 2/Taller 1/Algoritmo de Ordenamiento Java/Insercion.java
@@ -1,1 +1,52 @@
+import java.util.Random;
+
+public class Insercion {
+    
+    public static void insertionSort(int[] arr, int n, int[] comparisons, int[] swaps) {
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+
+            while (j >= 0 && arr[j] > key) {
+                comparisons[0]++;
+                arr[j + 1] = arr[j];
+                j = j - 1;
+                swaps[0]++;
+            }
+            arr[j + 1] = key;
+            swaps[0]++;
+        }
+    }
+
+    public static void main(String[] args) {
+        final int N = 1000;
+        int[] arr = new int[N];
+        int[] comparisons = {0};
+        int[] swaps = {0};
+
+        Random rand = new Random();
+        for (int i = 0; i < N; i++) {
+            arr[i] = rand.nextInt(10);  
+        }
+
+        System.out.println("Números generados:");
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        insertionSort(arr, N, comparisons, swaps);
+
+        System.out.println("Numeros ordenados:");
+        for (int i = 0; i < N; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
+        System.out.println("Comparaciones: " + comparisons[0]);
+        System.out.println("Intercambios: " + swaps[0]);
+        System.out.println("Estable: Si");
+        System.out.println("Insercion: Si");
+    }
+}
 
